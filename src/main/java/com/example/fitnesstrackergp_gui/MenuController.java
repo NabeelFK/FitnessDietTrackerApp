384e4bb0c6c7817e69e39d5563c6088320b6de39
shiva <shivashrita.das@ucalgary.ca>
2025-04-17 10:38:14 -0600

    worked on the exit button just need to update the pom

--- src/main/java/com/example/fitnesstrackergp_gui/MenuController.java
+++ src/main/java/com/example/fitnesstrackergp_gui/MenuController.java
@@ -1,5 +1,6 @@
 package com.example.fitnesstrackergp_gui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class MenuController {
 
@@ -33,5 +35,16 @@ public class MenuController {
         stage.setTitle("Create New Account");
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    @FXML
+    public void handleExitBtn(ActionEvent actionevent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Are you sure you wish to exit?");
+        Optional <ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() ==ButtonType.OK) {
+            Platform.exit();
+        }
     }
 }
